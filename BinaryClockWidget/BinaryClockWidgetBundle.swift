9672192f7a13639a3a539a8517a6f26768edd432
chanwoo040531 <chanwoo040531@gmail.com>
2023-10-04 01:57:45 +0900

    binary clock widget application

---
+++ BinaryClockWidget/BinaryClockWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  BinaryClockWidgetBundle.swift
+//  BinaryClockWidget
+//
+//  Created by 김찬우 on 10/3/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct BinaryClockWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        BinaryClockWidget()
+    }
+}
